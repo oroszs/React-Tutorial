bdfd72f547d78c1afb7ee887122e3e7e6d4d584b
Sean Orosz <seanorosz@yahoo.com>
2021-11-26 15:11:15 -0500

    V1.02

--- src/index.js
+++ src/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+function Square (props){
+  return (
+    <button className="square"
+    onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
@@ -57,6 +74,10 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+const handleClick = (index) => {
+
 }
 
 // ========================================
