0b4ec27cfa59ae88daaeafe057c40856ab3916c6
Sean Orosz <seanorosz@yahoo.com>
2021-11-28 13:19:50 -0500

    V1.1

--- src/index.js
+++ src/index.js
@@ -53,11 +53,19 @@ class Game extends React.Component {
         squares: Array(9).fill(null)
       }],
       xIsNext: true,
+      stepNumber: 0,
     }
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]){
@@ -69,12 +77,13 @@ class Game extends React.Component {
         squares: squares,
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -82,7 +91,7 @@ class Game extends React.Component {
       `Go to Move #${move}` :
       `Go to Game Start`;
       return(
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
